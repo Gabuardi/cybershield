0f3b215ad5ecf6159532b73bce6aeb8bff1bcb81
Josué Gabuardi <gabuardi@ibm.com>
2024-08-13 01:14:26 -0600

    Create new endppoint

--- rest_api/adapters/user_adapters.py
+++ rest_api/adapters/user_adapters.py
@@ -1,4 +1,6 @@
+import base64
 import hashlib
+from fastapi import Request
 
 
 def encrypt_password(password: str) -> str:
@@ -11,3 +13,15 @@ def user_in_org_adapter(data: list) -> list[int]:
         for nested in row:
             result.append(nested["org_id"])
     return result
+
+
+def get_auth_header(request: Request):
+    header = request.headers.get("Authorization")
+    get_auth_credentials = header.split(" ")[1]
+    decoded_credentials = base64.b64decode(get_auth_credentials).decode(
+        "utf-8")
+    credentials = decoded_credentials.split(":")
+    return {
+        "email": credentials[0],
+        "password": credentials[1]
+    }
--- rest_api/main.py
+++ rest_api/main.py
@@ -1,28 +1,27 @@
-import psycopg2
 from fastapi import FastAPI, Request, HTTPException
-from config_params import ConfigParams
-from operations.user import UserOperations
-import base64
+from rest_api.config_params import ConfigParams
+from rest_api.operations.user import UserOperations
+
+from rest_api.adapters.user_adapters import get_auth_header
 
 app = FastAPI()
 db_config = ConfigParams().db_params
 
-
-def get_auth_header(request: Request):
-    header = request.headers.get("Authorization")
-    get_auth_credentials = header.split(" ")[1]
-    decoded_credentials = base64.b64decode(get_auth_credentials).decode(
-        "utf-8")
-    credentials = decoded_credentials.split(":")
-    return {
-        "email": credentials[0],
-        "password": credentials[1]
-    }
+user_ops = UserOperations(db_config)
 
 
+# ----------------------------------------------------------------------------
+# USER
+# ----------------------------------------------------------------------------
 @app.get("/login", tags=["User Management"])
 def get_user_data(request: Request):
     credentials = get_auth_header(request)
-    if UserOperations(db_config).authenticate(**credentials):
-        return UserOperations(db_config).get_user_data("cr7@futbol.com")
+    if user_ops.authenticate(**credentials):
+        return user_ops.get_user_data("cr7@futbol.com")
     raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+@app.put('/password-update/{user_id}', tags=["User Management"])
+def update_user_password(user_id: int, body: dict, request: Request):
+    print(user_id)
+    print(body)
